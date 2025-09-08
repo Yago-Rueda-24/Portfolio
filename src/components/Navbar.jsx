9c1c488ddbf7a4dfe66bd4c4a8a690fbf619f82a
yago-vasco <yago.rc.04@gmail.com>
2025-09-08 21:47:33 +0200

    modificacion de la barra de navegación

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,11 @@ function Navbar() {
   return (
     <nav className="navbar">
       <a href="#home">Inicio</a>
-      <a href="#estudios">Estudios</a>
       <a href="#projects">Proyectos</a>
-      <a href="#contact">Contacto</a>
+      <a href="#stackTecnologico">Stack Tecnologico</a>
+      <a href="#experience">Experiencia</a>
+      <a href="#estudios">Estudios</a>
+      
     </nav>
   );
 }
